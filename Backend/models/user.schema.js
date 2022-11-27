9f36702bc0374f391411acb99bd4a2f241d2e7cf
Rajitkumar <krajit909@gmail.com>
2022-11-27 12:05:37 +0530

    modified

--- Backend/models/user.schema.js
+++ Backend/models/user.schema.js
@@ -6,8 +6,12 @@ const userSchema = mongoose.Schema(
             type: String,
             required: [true, "Name is required"],
             maxLength:[50, "Name must be less then 50"],
-
         },
-        
+        email: {
+            type: String,
+            required: [true, "Email is required"],
+            maxLength:[8, "Email must be atleast 8 Character"],
+            select: false
+        }
     }
 )
