f7629bf0c021b35ac19e3c4661796380ba3eddad
Rajitkumar <krajit909@gmail.com>
2023-01-09 22:43:24 +0530

    updated

--- Backend/config/razorpay.config.js
+++ Backend/config/razorpay.config.js
@@ -2,6 +2,8 @@ import Razorpay from 'razorpay'
 import config from './index.js'
 
 const razorpay = new Razorpay({
+
     key_id: config.RAZORPAY_KEY_ID,
     key_secret: config.RAZORPAY_SECRET
+    
 })
